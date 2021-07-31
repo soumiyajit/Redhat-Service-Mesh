6e8aeafe6ce6f4af2a2e4c679daabc58427494e4
Soumiyajit <soumiyajit@Soumiyajits-MacBook-Pro.local>
2021-07-31 17:17:00 +0530

    adding the sample app

---
+++ service-mesh-sample-app/service-mesh-sample-v1/go.mod
@@ -0,0 +1,3 @@
+module github.com/soumiyajit/Redhat-Service-Mesh/service-mesh-sample-app/service-mesh-sample-v1
+
+go 1.16
---
+++ service-mesh-sample-app/service-mesh-sample-v2/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/api/helloworld", helloworldHandler)
+	http.HandleFunc("/api/v2/helloiworld", helloworldHandler)
+
+	http.ListenAndServe(port(), nil)
+}
+
+func port() string {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+	return ":" + port
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Redhat Service Mesh by Soumiyajit")
+}
+
+type helloworld struct {
+	Message string
+	Version string
+}
+
+func helloworldHandler(w http.ResponseWriter, r *http.Request) {
+
+	m := helloworld{"Service Mesh Sample App by Soumiyajit.", "v2"}
+	b, err := json.Marshal(m)
+
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Add("Content-Type", "application/json; charset=utf-8")
+	w.Write(b)
+}
